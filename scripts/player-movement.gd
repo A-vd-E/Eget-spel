e58c36a82479e1416d23eccc8e18c0fbb6f8d289
avde <alexander.vonderemden@gmail.com>
2026-04-16 14:03:21 +0200

    Mapped movement to specific input keys and added a dash movement. Fixes #4, #9

---
+++ scripts/player-movement.gd
@@ -0,0 +1,58 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -600.0
+
+const DASH_SPEED = 900.0
+var dashing = false
+var can_dash = true
+
+
+func _physics_process(delta: float) -> void:
+	
+	# Add the gravity.
+	if not is_on_floor() and !dashing:
+		velocity += get_gravity() * delta
+
+	# Handles dash
+	if Input.is_action_just_pressed("dash") and can_dash:
+		dash()
+	
+	# Handles basic horizontal movement and jumping
+	if not dashing:
+		basic_movement()
+
+	move_and_slide()
+
+func basic_movement():
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor() and !dashing:
+		velocity.y = JUMP_VELOCITY
+	
+	# Get the input direction and handle the movement/deceleration.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+		
+# Dashes forward in the direction player is moving, 
+# is then placed on cooldown		
+func dash():
+	var dash_direction := Input.get_axis("left", "right")
+	dashing = true
+	can_dash = false
+	# Dash duration
+	$dash_timer.start()
+	# Dash cooldown
+	$dash_again_timer.start()
+	velocity.x =  dash_direction * DASH_SPEED 
+	velocity.y = 0
+
+func _on_dash_timer_timeout() -> void:
+	dashing = false
+
+func _on_dash_again_timer_timeout() -> void:
+	can_dash = true
