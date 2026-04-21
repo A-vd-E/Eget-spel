d3c551b606d64d35b4d8e3aa58645193d0f61063
Malte Björs <maltebjors@gmail.com>
2026-04-21 13:30:12 +0200

    Added lerp to player position so it will look smoother with lag.

--- scripts/player-movement.gd
+++ scripts/player-movement.gd
@@ -8,6 +8,9 @@ const DASH_SPEED = 900.0
 var dashing = false
 var can_dash = true
 
+@export var sync_position: Vector2
+@export var lerp_weight = 0.75
+
 func _enter_tree():
 		set_multiplayer_authority(name.to_int())
 
@@ -17,6 +20,11 @@ func _ready():
 	else:
 		$Camera2D.enabled = false
 
+func _process(delta: float):
+	if is_multiplayer_authority():
+		sync_position = global_position
+	else:
+		global_position = global_position.lerp(sync_position, lerp_weight)
 
 func _physics_process(delta: float) -> void:
 	if not is_multiplayer_authority(): return
