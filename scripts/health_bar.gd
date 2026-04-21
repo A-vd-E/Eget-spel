eb86698190ebd0399aa29c408fa52ad695de90b0
avde <alexander.vonderemden@gmail.com>
2026-04-21 23:28:56 +0200

    Synced the health system. Damage is currently caused by a key press as a test to see if system works, will need to be remade into a proper damage system. Fixes #13

--- scripts/health_bar.gd
+++ scripts/health_bar.gd
@@ -3,7 +3,10 @@ extends ProgressBar
 @export var health: Node 
 
 
+# sets max and current values for hp bar based on 
+# character hp
 func _ready() -> void:
+	
 	max_value = health.max_health
 	value = health.max_health
 	health.health_changed.connect(_on_health_changed)
